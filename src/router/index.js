82cb724024991dd6c602ad170094eb9504124203
Tony <chuntatwong0@gmail.com>
2019-03-17 03:13:30 +0800

    completed login and register function

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/Users/Login'
+import Register from '@/components/Users/Register'
+import Home from '@/components/Home'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -8,8 +11,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
     }
   ]
 })
