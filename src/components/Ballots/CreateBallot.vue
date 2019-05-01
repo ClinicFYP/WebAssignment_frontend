bd3b903bdba6d4b78e3e3988f805e6b3d57768f1
Tony <chuntatwong0@gmail.com>
2019-05-01 22:51:53 +0800

    fix heroku

--- src/components/Ballots/CreateBallot.vue
+++ src/components/Ballots/CreateBallot.vue
@@ -265,9 +265,9 @@
 <script>
 import { DateTime as LuxonDateTime } from 'luxon'
 import 'vue-datetime/dist/vue-datetime.css'
-const { CandidateServices } = require('Services/CandidateServices')
-const { BallotServices } = require('Services/BallotServices')
-const { UserServices } = require('Services/UserServices')
+const { CandidateServices } = require('CandidateServices')
+const { BallotServices } = require('BallotServices')
+const { UserServices } = require('UserServices')
 import { required } from 'vuelidate/lib/validators'
 import { Datetime } from 'vue-datetime'
 export default {
