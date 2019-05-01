56663c12eadf22d7ff4a6dc23b558c3320b7d5b2
Tony <chuntatwong0@gmail.com>
2019-05-02 00:06:10 +0800

    fix heroku

--- src/components/Ballots/Ballot.vue
+++ src/components/Ballots/Ballot.vue
@@ -144,7 +144,7 @@
 </template>
 
 <script>
-import { BallotServices } from '~/Services/BallotServices'
+import { BallotServices } from 'src/Services/BallotServices'
 // import { unescape } from 'querystring'
 const moment = require('moment')
 export default {
