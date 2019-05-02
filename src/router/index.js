5ef2daf261fbaaa233a5e177492d12ac709c1192
Tony <chuntatwong0@gmail.com>
2019-05-02 17:40:29 +0800

    handle 404

--- src/router/index.js
+++ src/router/index.js
@@ -87,7 +87,7 @@ export default new Router({
         },
         {
           path: '*',
-          component: Layout
+          component: Home
         }
       ]
     },
@@ -118,7 +118,7 @@ export default new Router({
     },
     {
       path: '*',
-      component: Layout
+      component: Login
     }
   ]
 })
